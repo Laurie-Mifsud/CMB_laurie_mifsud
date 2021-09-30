8384fc3f67df87e6257206632840ac84d7e38300
MIFSUD Laurie <m18024598@V-PP-24-006.salsa.univ-amu.fr>
2021-09-30 18:39:50 +0200

    update : step 50 ok

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -5,39 +5,33 @@ Spyder Editor
 This is a temporary script file.
 """
 import copy as cp 
+import matplotlib.pyplot as plt
 
 my_file=open("/amuhome/m18024598/CMB/TP2/input","r")
 data = my_file.read()
 my_file.close ()
-
 data_splitted = data.split("\n")
-#print(data_splitted)
-#print(data)
+print(data)
 
 
-N0=len(data_splitted)
-N1=len(data_splitted[0])
-
-#array = [[int(data_splitted[i][j]) for j in  range(N1)] for i in  range(N0-1)]
-#print("array=",array)
+n=len(data_splitted[:][1])
+m=len(data_splitted[0])
+array = [[int(data_splitted[line][column]) for  column  in  range(m)] for  line in  range(n)]
 
 
-array2 = [[int(data_splitted[line][column]) for  column  in  range(N1)] for  line in  range(N0-1)]
-#print(array2)
-
-def count_neighbours(i,j,matrice) :
-    sum_nbours = sum(matrice[i-1][j-1:j+2])
-    sum_nbours = sum_nbours + sum(matrice[i][j-1:j+2]) - matrice[i][j]
-    sum_nbours = sum_nbours + sum(matrice[i+1][j-1:j+2])
+def count_neighbours(i,j,array) :
+    sum_nbours = (
+        array[i-1][j-1] + array[i-1][j] + array[i-1][j+1] 
+        + array[i][j-1] + array[i][j+1] 
+        + array[i+1][j-1] + array[i+1][j] + array[i+1][j+1]
+        )
     return sum_nbours
-
-
-n=len(array2)
-m=len(array2[0])
+print(count_neighbours(3, 4, array))
 
 for i in range(1,n-1):
     for j in range(1,m-1):
-        sum_nbours = count_neighbours(i, j, array2)
+        sum_nbours = count_neighbours(i, j, array)
+    
         
 def cell_next_state(state,sum_nbours) :
     if state == 0:
@@ -49,27 +43,24 @@ def cell_next_state(state,sum_nbours) :
             return 0
         return 1
 
-def next_state(matrice):
-    array3 = [] 
+
+def next_state(array):
+    array2 = cp.deepcopy(array)
     for i in range(1,n-1):
-        line = []
         for j in range(1,m-1):
-            next = cell_next_state(matrice[i][j], count_neighbours(i, j, array2))
-            line.append(next)
-        array3.append(line)
-    return array3
+            array2[i][j]= cell_next_state(array[i][j], count_neighbours(i, j, array))
+    return array2
 
-print(next_state(array2))
+def repetition(array,k):
+    for t in range(k):
+        array = next_state(array)
+    return array
 
-def repetition(matrice,k):
-    matrice2 = matrice.copy()
-    for n in range(k):
-        matrice2 = next_state(matrice2)
-    return matrice2
+print(repetition(array, 2))
 
-print(repetition(array2, 49))
-
-
+m = repetition(array, 50)
+plt.imshow(m)
+plt.show()
     
 
     
